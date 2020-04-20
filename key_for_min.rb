187a300e0f8de0daddcddc7fe2b8a7a079bd70c5
Philippe Timothe <philippetimothe@gmail.com>
2020-04-20 21:55:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = Float::INFINITY
   name_hash.collect do |key, value|
     if value < lowest_value
-     
+      lowest_value = value
       lowest_key = key
     end
   end
